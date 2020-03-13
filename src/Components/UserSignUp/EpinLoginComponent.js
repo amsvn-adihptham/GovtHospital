d340f8f8fd8990b8731ed8e3db7b1d17d058bec6
Amal Salvin <amalsalvin97@gmail.com>
2020-03-13 22:54:27 +0530

    🚀 UI updated: ePIN Login
    Probably the final UI of Login Component
    
    PS: added a package 'react-native-smooth-pincode-input'

--- src/Components/UserSignUp/EpinLoginComponent.js
+++ src/Components/UserSignUp/EpinLoginComponent.js
@@ -3,19 +3,24 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput,
-  KeyboardAvoidingView,
+  Image,
   Dimensions,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SmoothPinCodeInput from 'react-native-smooth-pincode-input';
 
 const color = {
   body: '#41CCC9',
-  highlight: '#41CCC9',
+  highlight: '#4D98CF',
   shadow: '#5A5386',
   button: '#524364',
   white: '#FFFFFF',
   black: '#000000',
+  wrong: '#900',
+  right: '#090',
 };
 
 const grid = {
@@ -33,37 +38,119 @@ const grid = {
   lowOpacity: 0.4,
   mediumOpacity: 0.6,
   highOpacity: 0.8,
-  // screenWidth: Math.round(Dimensions.get('window').width),
   screenWidth: Math.round(Dimensions.get('screen').width),
-  // screenHeight: Math.round(Dimensions.get('window').height),
   screenHeight: Math.round(Dimensions.get('screen').height),
   statusbarHeight: StatusBar.currentHeight,
+  forgotIcon: 'phonelink-lock',
+  logoImage_Http_URL: {
+    uri:
+      'https://cdn.clipart.email/7dd95b6e86cfb39037fa78c2f06a92fd_guidelines-to-use-survey-maps_1571-1600.png',
+  },
+  totalAttempts: 5,
 };
 
 export default class EpinLoginComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      code: '',
+      attempts: grid.totalAttempts,
+      attemptMessage: '',
+      attemptMessage_Color: color.white,
+    };
   }
 
+  pinInput = React.createRef();
+
+  _checkCode = pin => {
+    // console.log(`PIN is ${pin}`);
+    if (pin !== '1234') {
+      this.pinInput.current.shake().then(() => this.setState({code: ''}));
+      if (this.state.attempts > 1) {
+        let {attempts} = this.state;
+        attempts = attempts - 1;
+        this.setState({
+          attempts,
+          attemptMessage: `You have ${attempts} more ${
+            attempts !== 1 ? 'tries' : 'chance'
+          }!`,
+          attemptMessage_Color: color.wrong,
+        });
+      } else {
+        this.setState({
+          attempts: grid.totalAttempts,
+          attemptMessage: 'WRONG AGAIN ! You are LOCKED',
+          attemptMessage_Color: color.wrong,
+        });
+      }
+    } else {
+      this.setState({
+        code: '',
+        attempts: grid.totalAttempts,
+        attemptMessage: 'SUCCESS! You are LoggedIn',
+        attemptMessage_Color: color.right,
+      });
+    }
+  };
+
+  handleTextChange = code => {
+    this.setState({code});
+    // console.log(code);
+  };
+
   render() {
+    const {code, attemptMessage, attemptMessage_Color} = this.state;
+
     return (
       <View style={styles.viewport}>
         <StatusBar backgroundColor={color.shadow} barStyle="light-content" />
         <View style={styles.container}>
           {/* LOGO AREA for Logo view */}
           <View style={styles.logoview}>
-            <Text style={styles.logoviewtext}>
-              {grid.screenHeight} x {grid.screenWidth} *LOGO*
-            </Text>
+            <View>
+              <Image source={grid.logoImage_Http_URL} style={styles.logo} />
+            </View>
           </View>
           {/* CENTER AREA for PIN view */}
           <View style={styles.pinview}>
-            <TextInput placeholder="PIN" style={styles.pinviewtext} />
+            <View style={styles.pinviewContent}>
+              <Text style={styles.pintitle}>Enter your ePIN</Text>
+              <SmoothPinCodeInput
+                ref={this.pinInput}
+                placeholder={<View style={styles.pinInputPlaceholder} />}
+                mask={<View style={styles.pinInputMask} />}
+                maskDelay={10}
+                codeLength={4}
+                password={true}
+                cellStyle={null}
+                cellStyleFocused={null}
+                value={code}
+                onTextChange={this.handleTextChange}
+                onFulfill={this._checkCode}
+                restrictToNumbers={true}
+              />
+            </View>
           </View>
           {/* FOOTER AREA for Footer view */}
           <View style={styles.footerview}>
-            <TextInput placeholder="Footer" style={styles.footerviewtext} />
+            <View style={styles.footerSectionTop}>
+              <Text
+                style={[styles.attempttitle, {color: attemptMessage_Color}]}>
+                {attemptMessage}
+              </Text>
+            </View>
+            <View style={styles.footerSectionBottom}>
+              <TouchableOpacity
+                style={styles.footerSubSection}
+                onPress={() => console.warn('Forgot ePIN Screen')}>
+                <Icon
+                  name={grid.forgotIcon}
+                  size={grid.unit * 2.25}
+                  color={color.shadow}
+                />
+                <Text style={styles.footer_ForgotText}>Forgot your ePIN?</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -80,22 +167,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderColor: '#333',
-    borderWidth: 5,
   },
   logoview: {
     alignItems: 'center',
     justifyContent: 'center',
-    height: '45%',
-    borderWidth: 5,
+    height: '42%',
   },
   pinview: {
     justifyContent: 'center',
-    height: '10%',
+    height: '16%',
   },
   footerview: {
+    alignItems: 'center',
     justifyContent: 'flex-end',
-    height: '45%',
-    borderWidth: 5,
+    height: '42%',
   },
   logoviewtext: {
     height: 50,
@@ -106,18 +191,58 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     alignItems: 'flex-end',
   },
-  pinviewtext: {
-    height: 50,
-    fontSize: 24,
-    borderColor: '#999',
-    borderWidth: 5,
-    textAlign: 'center',
+  pinviewContent: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
-  footerviewtext: {
-    height: 50,
-    fontSize: 24,
-    borderColor: '#999',
-    borderWidth: 5,
-    textAlign: 'center',
+  footerSectionTop: {
+    height: '50%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  footerSectionBottom: {
+    height: '50%',
+    width: '100%',
+    justifyContent: 'flex-end',
+  },
+  footerSubSection: {
+    alignItems: 'center',
+    marginBottom: '5%',
+  },
+  footer_ForgotText: {
+    fontSize: grid.unit * 1.25,
+    textAlignVertical: 'center',
+    color: color.shadow,
+  },
+  pintitle: {
+    color: color.shadow,
+    fontSize: grid.unit * 1.5,
+    marginBottom: 8,
+  },
+  attempttitle: {
+    color: color.wrong,
+    fontSize: grid.unit * 1.5,
+    marginBottom: 8,
+  },
+  logo: {
+    height: 200,
+    width: 200,
+    resizeMode: 'cover',
+  },
+  pinInputPlaceholder: {
+    width: 45,
+    height: 45,
+    borderRadius: 10,
+    opacity: 0.3,
+    backgroundColor: color.highlight,
+    borderColor: color.shadow,
+    borderWidth: 2,
+  },
+  pinInputMask: {
+    width: 45,
+    height: 45,
+    borderRadius: 5,
+    backgroundColor: color.highlight,
   },
 });
